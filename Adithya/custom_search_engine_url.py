178d472bda7677f40b5fd9792c4123abf9901b1f
adithya-anand-999 <adhitech17@gmail.com>
2025-04-14 22:55:36 -0400

    clean up code

--- Adithya/custom_search_engine_url.py
+++ Adithya/custom_search_engine_url.py
@@ -1,12 +1,5 @@
 # This is a code file for working on the custom search engine google API to create a more robust system for URL collections. 
- 
- # <script async src="https://cse.google.com/cse.js?cx=e04e3429da6a744ab">
- # </script>
- # <div class="gcse-search"></div>
- 
 import requests
-# import json
-# import string
 import openpyxl
 import time
 
@@ -27,18 +20,18 @@ def custom_SE(addr, key=API_KEY, cx=CX):
 
 
 # simple tests of the custom_SE function
-addrs = ["6 STANDISH CIR, ANDOVER, MA, 01810", "150 TRAINCROFT ST, MEDFORD, MA, 02155"]
-custom_SE(addrs[1])
+# addrs = ["6 STANDISH CIR, ANDOVER, MA, 01810", "150 TRAINCROFT ST, MEDFORD, MA, 02155"]
+# custom_SE(addrs[1])
 
+# code updates excel file res-econ_RA_data.xlsx. Commented out as no reason to run code more than once. All urls have been collected.
+# wb = openpyxl.load_workbook('./res-econ_RA_data.xlsx')
+# ws = wb.active
 
-wb = openpyxl.load_workbook('./res-econ_RA_data.xlsx')
-ws = wb.active
+# for row in range(1,ws.max_row+1):
+#     address = ws.cell(row=row, column=1).value
+#     url = custom_SE(address)
+#     ws.cell(row=row, column=7, value=url)
+#     wb.save('res-econ_RA_data.xlsx')
+#     time.sleep(1)
 
-for row in range(1,ws.max_row+1):
-    address = ws.cell(row=row, column=1).value
-    url = custom_SE(address)
-    ws.cell(row=row, column=7, value=url)
-    wb.save('res-econ_RA_data.xlsx')
-    time.sleep(1)
-
-print("Done updating Excel file.")
+# print("Done updating Excel file.")
