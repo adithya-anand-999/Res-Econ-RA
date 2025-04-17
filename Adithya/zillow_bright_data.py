cdeb5a3ac5e29e520b183dd1c7cbb6b2077d17af
adithya-anand-999 <adhitech17@gmail.com>
2025-04-17 13:54:58 -0400

    comments to code, updated excel doc

--- Adithya/zillow_bright_data.py
+++ Adithya/zillow_bright_data.py
@@ -1,17 +1,15 @@
+# required libraries we use.
 import requests
 from time import sleep
-import json
-import pandas as pd
 import openpyxl
 
+# data points we are collecting
 required_keys = ["zestimate", "taxAssessedValue", "dateSoldString", "livingArea", "yearBuilt", "lotAreaValue", "lotAreaUnits"]
-# website_3_data_path = r"./Adithya/website_3_data.json"
-# with open(website_3_data_path, "r") as file:
-#     content = file.read().strip()
-#     website_3_data = None if not content else json.load(file)
-# print(website_3_data)
 
-def zillow_api_call(addr):
+# function which converts our url for an address to a unique snapshot id. More information about snapshot ID can be found <link>, it is a step in the data collection
+# framework found for Bright Data. Bright Data provides a third party zillow API we are using to collect information. 
+# TODO: write comments explaining how the code in the function works.
+def zillow_api_call(addr_url):
     url = "https://api.brightdata.com/datasets/v3/trigger"
     headers = {
         "Authorization": "Bearer 13045b9674ca543e98b3768901c6799a2b876aff0220e26d61c9dae63dfeec0a",
@@ -22,38 +20,42 @@ def zillow_api_call(addr):
         "include_errors": "true",
     }
     data = [
-        {"url":addr},
+        {"url":addr_url},
     ]
     response = requests.post(url, headers=headers, params=params, json=data)
     return(response.json()['snapshot_id'])
 
+# function parses the snapshot ID, producing a json object with all the data for the address. From this we collect the specific data points required.
 
+# TODO: write comments explaining how the code in the function works.
 def snapshot_id_parse(snapshotID):
-    snapshot_url = f"https://api.brightdata.com/datasets/v3/snapshot/{snapshotID}"
-    snapshot_headers = {"Authorization": "Bearer 13045b9674ca543e98b3768901c6799a2b876aff0220e26d61c9dae63dfeec0a"}
-    snapshot_data = requests.request('GET', snapshot_url, headers=snapshot_headers).json()
+    url = f"https://api.brightdata.com/datasets/v3/snapshot/{snapshotID}"
+    headers = {"Authorization": "Bearer 13045b9674ca543e98b3768901c6799a2b876aff0220e26d61c9dae63dfeec0a"}
+    data = requests.request('GET', url, headers=headers).json()
     retry_count = 0
 
-    while 'status' in snapshot_data and retry_count < 3:
+    while 'status' in data and retry_count < 3:
         retry_count += 1
         print(f"Retrying {retry_count}, sleeping 30s…")
         sleep(30)
-        snapshot_data = requests.get(snapshot_url, headers=snapshot_headers).json()
+        data = requests.request('GET', url, headers=headers).json()
 
-    if 'status' in snapshot_data:
+    if 'status' in data:
         print(f"Scraping failed for snapshot {snapshotID}")
         return None
     
-    # print(snapshot_data)
-    # readable_output = snapshot_data
+    # print(data)
+
     payload = {}
     for key in required_keys:
-        payload[key] = snapshot_data.get(key, None)
-    print(payload)
-    payload["heating"], payload["cooling"] = None, None
-    if "interior_full" in snapshot_data:
-        print(1)
-        for dict in snapshot_data['interior_full']:
+        payload[key] = data.get(key, None)
+    
+    # print(payload)    
+    # print(data['interior_full'])
+
+    payload["heating"] = payload["cooling"] = None
+    if data["interior_full"] != None:
+        for dict in data['interior_full']:
             if dict['title'] == "Heating": payload['heating'] = dict['values'][0].split(": ")[1]
             if dict['title'] == "Cooling": payload['cooling'] = dict['values'][0].split(": ")[1]
     return payload
@@ -63,29 +65,31 @@ def snapshot_id_parse(snapshotID):
 # print(snapshot_id_parse(zillow_api_call("https://www.zillow.com/homedetails/2506-Gordon-Cir-South-Bend-IN-46635/77050198_zpid/")))
 # print(zillow_api_call("https://www.zillow.com/homedetails/2506-Gordon-Cir-South-Bend-IN-46635/77050198_zpid/"))
 
+# open our excel doc
 wb = openpyxl.load_workbook('./res-econ_RA_data.xlsx')
 ws = wb.active
 
-for row in range(11,13):
+# loops over the rows in our excel. Bounds are inclusive of first arg, exclusive of second arg. Use the row numbers found in the excel. So to include row 3 you
+# would set your first arg to 3.
+for row in range(5,6):
+    # grab the url for 'row' found in column 6
     url = ws.cell(row=row, column=6).value
-    print(url)
+    print(f"Collecting {url}")
+
+    # collect the data for the url
     data = snapshot_id_parse(zillow_api_call(url))
-    # print(data)
+
+    # if data collection for the url failed, snapshot_id_parse will return None. Then this conditional will run skipping this row as no data has been collected.
     if data == None: continue
+
+    # for each data point, write to a separate column in our excel, if the data point does not exist we write "None"
     for i,point in enumerate(data.values()):
         ws.cell(row=row, column=7+i, value=point if point else "None")
+    
+    # as changes have been made to the excel in the above code, save the changes
     wb.save('res-econ_RA_data.xlsx')
+    
     print(f"data collection for {url} done")
     sleep(5)
 
-
-# df = pd.read_excel('res-econ_RA_data.xlsx', engine='openpyxl')
-# column_6 = list(df.iloc[:, 5])[11:36]
-
-# for addr in column_6[:3]:
-#     website_3_data[addr] = snapshot_id_parse(zillow_api_call(addr))
-
-# with open(website_3_data_path, "w") as f:
-#     json.dump(website_3_data, f, indent=4)
-
-# print("done writing and scraping")
+print("Data collection finished!")
