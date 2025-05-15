3c7b7e04edb1ea1d1fa836019aaca308fa8bdcc5
adithya-anand-999 <adhitech17@gmail.com>
2025-05-15 18:48:23 -0400

    change in writing zillow data logic

--- data.py
+++ data.py
@@ -2,8 +2,8 @@ from coordinates import get_coordinates
 from capacity_factor import get_capacity_factor
 from roof_space import get_roof_space
 from zillow_url import get_zillow_url
-# from zillow_data import get_zillow_data
-# from zillow_data import get_snapshot_id
+from zillow_data import get_zillow_data
+from zillow_data import get_snapshot_id
 
 import openpyxl
 import asyncio
@@ -24,7 +24,10 @@ def get_data(given_excel_path, new_excel_name=None):
             ws.cell(row=row, column=9, value=asyncio.run(get_roof_space(addr.split(" ")[0],lat,lon)))
             url = get_zillow_url(addr)
             ws.cell(row=row, column=10, value=url)
-            # ws.cell(row=row, column=11, value=get_zillow_data(get_snapshot_id(url)))
+            zillow_data = get_zillow_data(get_snapshot_id(url))
+            if not zillow_data: continue
+            for i,point in enumerate(zillow_data.values()):
+                ws.cell(row=row, column=11+i, value=point if point else "None")
             wb.save(given_excel_path)
             time.sleep(1)
         print(f"Row {row} completed")
