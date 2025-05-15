b02705bf7992dcfa55efc574ed93532ca4115631
elizabethkob <obrien.elizabethkennedy@gmail.com>
2025-05-15 19:05:14 -0400

    data.py test again

--- data.py
+++ data.py
@@ -2,8 +2,8 @@ from coordinates import get_coordinates
 from capacity_factor import get_capacity_factor
 from roof_space import get_roof_space
 from zillow_url import get_zillow_url
-from zillow_data import get_zillow_data
-from zillow_data import get_snapshot_id
+# from zillow_data import get_zillow_data
+# from zillow_data import get_snapshot_id
 
 import openpyxl
 import asyncio
@@ -16,18 +16,19 @@ def get_data(given_excel_path, new_excel_name=None):
 
     num_rows = ws.max_row
 
-    for row in range(2,num_rows+1):
+    for row in range(205,num_rows+1):
         addr = ws.cell(row=row, column=1).value
         if addr:
+            print(addr)
             lat, lon = get_coordinates(addr)
             ws.cell(row=row, column=8, value=get_capacity_factor(lat,lon))
             ws.cell(row=row, column=9, value=asyncio.run(get_roof_space(addr.split(" ")[0],lat,lon)))
             url = get_zillow_url(addr)
             ws.cell(row=row, column=10, value=url)
-            zillow_data = get_zillow_data(get_snapshot_id(url))
-            if not zillow_data: continue
-            for i,point in enumerate(zillow_data.values()):
-                ws.cell(row=row, column=11+i, value=point if point else "None")
+            # zillow_data = get_zillow_data(get_snapshot_id(url))
+            # if not zillow_data: continue
+            # for i,point in enumerate(zillow_data.values()):
+            #     ws.cell(row=row, column=11+i, value=point if point else "None")
             wb.save(given_excel_path)
             time.sleep(1)
         print(f"Row {row} completed")
